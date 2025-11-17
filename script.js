6fc20a46071663c6bfb9e5c7c9cca51e51a3fe9b
Jan Altenhof <jan.altenhof12@gmx.de>
2025-11-18 00:13:24 +0100

    fixed problems

--- script.js
+++ script.js
@@ -26,18 +26,7 @@ function accesInnerHTML() {
     }
 };
 
-function renderPhotos(ArrayPosition) {
-    return `                
-                            <img class="Photos" 
-                            role ="button" aria-label="Picture of `+PHOTO_COLLECTION[ArrayPosition]+`" aria-haspopup="dialog" aria-controls="Dialog"
-                            onkeypress=openDialog(`+ArrayPosition+`) 
-                            onclick=openDialog(`+ArrayPosition+`) 
-                            tabindex="0"
-                            src="./img/`+PHOTO_COLLECTION[ArrayPosition]+`" 
-                            alt="`+PHOTO_COLLECTION[ArrayPosition]+`">
-                            </img>`;
-                            
-};
+
 
 /* Opening and closing the Dialog */
 
@@ -84,33 +73,6 @@ function accesInnerDialog(ArrayPosition) {
     DIALOG_CONTENT.innerHTML += innerDialogNav(ArrayPosition);
 };
 
-/* X image to close the Dialog */
 
-function innerDialog(ArrayPosition) {
-    return `                    <header class="DialogHeader">
-                                    <h2 id="dialog-title" class="DialogTitle">`
-                                        +PHOTO_COLLECTION[ArrayPosition]+
-                                    `</h2>
-                                    <img class="DialogClose" id="dialog-close" tabindex="0" onkeypress=closeDialog() onclick=closeDialog() src="img/close.svg" role="button" aria-label="close Dialog" aria-controls="Dialog" alt="X">
-                                    </img>
-                                </header>
-                                <main class="DialogMain">
-                                    <img class="DialogPhotos" src="./img/`+PHOTO_COLLECTION[ArrayPosition]+`" alt="`+PHOTO_COLLECTION[ArrayPosition]+`">
-                                    </img>
-                                </main>`;
-};
 
-/* Arrow images to navigate the Photo Collection */
-
-function innerDialogNav(ArrayPosition) {
-    return  `               <nav class="DialogNav">
-                                <img class="ArrowNavigation" id="previous-picture" tabindex="0" onkeypress=accesInnerDialog(`+(ArrayPosition-1)+`) onclick=accesInnerDialog(`+(ArrayPosition-1)+`) src="./img/arrow-left.svg" role="button" aria-label="previous picture"  alt="Arrow Left">
-                                </img>
-                                <p class="CurrentPosition">`
-                                    +(ArrayPosition + 1)+'/'+PHOTO_COLLECTION.length+
-                                `</p>
-                                <img class="ArrowNavigation" id="next-picture" tabindex="0" onkeypress=accesInnerDialog(`+(ArrayPosition+1)+`) onclick=accesInnerDialog(`+(ArrayPosition+1)+`) src="./img/arrow-right.svg" role="button" aria-label="next picture" alt="Arrow Right">
-                                </img>
-                            </nav>`;
-};
 
