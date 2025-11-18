33b278041321aa7fc469fe70256d3f0f3feb9daf
Jan Altenhof <jan.altenhof12@gmx.de>
2025-11-18 23:56:36 +0100

    fixed problems

--- script.js
+++ script.js
@@ -18,61 +18,45 @@ const PHOTO_CONTENT = document.getElementById("photo-collection");
 const DIALOG_CONTENT = document.getElementById("dialog");
 
 /* Generating the Photo Collection */
-
 function accesInnerHTML() {
     for (let ArrayPosition = 0; ArrayPosition < PHOTO_COLLECTION.length; ArrayPosition++) {
-        
         PHOTO_CONTENT.innerHTML += renderPhotos(ArrayPosition);
     }
 };
 
-
-
 /* Opening and closing the Dialog */
-
 function openDialog(ArrayPosition) {
-    
     DIALOG_CONTENT.showModal();
-    
     accesInnerDialog(ArrayPosition);
-    
+    focusOnDialogOpen();
 };
 
 function closeDialog() {
     DIALOG_CONTENT.close();
 };
 
-
-
 /* closing Dialog by clicking outside of it */
-
 DIALOG_CONTENT.addEventListener('click', (event) => {
-
     if (event.target == DIALOG_CONTENT) {
-        
         DIALOG_CONTENT.close();
     }
-    
 });
 
 /* Switch from last to first image and reverse + Generating the Dialog Content */
-
 function accesInnerDialog(ArrayPosition) {
-    
     if (ArrayPosition <= -1) {
-       
-        ArrayPosition = PHOTO_COLLECTION.length - 1;
+       ArrayPosition = PHOTO_COLLECTION.length - 1;
     }
-    
     else if (ArrayPosition >= PHOTO_COLLECTION.length) {
-       
-        ArrayPosition = 0;
+       ArrayPosition = 0;
     }
-
     DIALOG_CONTENT.innerHTML = innerDialog(ArrayPosition);
     DIALOG_CONTENT.innerHTML += innerDialogNav(ArrayPosition);
 };
 
-
-
+/*Focus on title of Dialog on opening the Dialog*/
+function focusOnDialogOpen() {
+    let DIALOG_TITLE = document.getElementById("dialog-title");
+    DIALOG_TITLE.focus();
+}
 
--- templateFunctions.js
+++ templateFunctions.js
@@ -1,43 +1,43 @@
 /*Generating the photo collection */
 function renderPhotos(ArrayPosition) {
-    return `                
-                            <img class="Photos" 
-                            role ="button" aria-label="Picture of `+PHOTO_COLLECTION[ArrayPosition]+`" aria-haspopup="dialog" aria-controls="dialog"
-                            onkeypress=openDialog(`+ArrayPosition+`) 
-                            onclick=openDialog(`+ArrayPosition+`) 
-                            tabindex="0"
-                            src="./img/`+PHOTO_COLLECTION[ArrayPosition]+`" 
-                            alt="`+PHOTO_COLLECTION[ArrayPosition]+`">
-                            </img>`;
+    return `<img class="Photos" 
+            role ="button" aria-label="Picture of `+PHOTO_COLLECTION[ArrayPosition]+`" aria-haspopup="dialog" aria-controls="dialog"
+            onkeypress=openDialog(`+ArrayPosition+`) 
+            onclick=openDialog(`+ArrayPosition+`) 
+            tabindex="0"
+            src="./img/`+PHOTO_COLLECTION[ArrayPosition]+`" 
+            alt="`+PHOTO_COLLECTION[ArrayPosition]+`">
+            </img>`;
                             
 };
 
 /*Generating header and main of the Dialog*/ 
 /* X image to close the Dialog */
 function innerDialog(ArrayPosition) {
-    return `                    <header class="DialogHeader">
-                                    <h2 id="dialog-title" class="DialogTitle">`
-                                        +PHOTO_COLLECTION[ArrayPosition]+
-                                    `</h2>
-                                    <img class="DialogClose" id="dialog-close" tabindex="0" onkeypress=closeDialog() onclick=closeDialog() src="img/close.svg" role="button" aria-label="close Dialog" aria-controls="Dialog" alt="X">
-                                    </img>
-                                </header>
-                                <main class="DialogMain">
-                                    <img class="DialogPhotos" src="./img/`+PHOTO_COLLECTION[ArrayPosition]+`" alt="`+PHOTO_COLLECTION[ArrayPosition]+`">
-                                    </img>
-                                </main>`;
+    return `<header class="DialogHeader">
+                <h2 id="dialog-title" class="DialogTitle" tabindex="-1">`
+                    +PHOTO_COLLECTION[ArrayPosition]+
+                `</h2>
+                <button class="DialogClose" id="dialog-close" onclick=closeDialog()>
+                    X
+                </button>
+            </header>
+            <img class="DialogPhotos" src="./img/`+PHOTO_COLLECTION[ArrayPosition]+`" alt="`+PHOTO_COLLECTION[ArrayPosition]+`">
+            </img>`;
 };
 
 /*Generating the Nav of the Dialog* 
 /* Arrow images to navigate the Photo Collection */
 function innerDialogNav(ArrayPosition) {
-    return  `               <nav class="DialogNav">
-                                <img class="ArrowNavigation" id="previous-picture" tabindex="0" onkeypress=accesInnerDialog(`+(ArrayPosition-1)+`) onclick=accesInnerDialog(`+(ArrayPosition-1)+`) src="./img/arrow-left.svg" role="button" aria-label="previous picture"  alt="Arrow Left">
-                                </img>
-                                <p class="CurrentPosition">`
-                                    +(ArrayPosition + 1)+'/'+PHOTO_COLLECTION.length+
-                                `</p>
-                                <img class="ArrowNavigation" id="next-picture" tabindex="0" onkeypress=accesInnerDialog(`+(ArrayPosition+1)+`) onclick=accesInnerDialog(`+(ArrayPosition+1)+`) src="./img/arrow-right.svg" role="button" aria-label="next picture" alt="Arrow Right">
-                                </img>
-                            </nav>`;
+    return  `<nav class="DialogNav">
+                <button class="ArrowNavigation" id="previous-picture" tabindex="-1" onclick=previousPicture(`+(ArrayPosition-1)+`) aria-label="previous picture">
+                    <<
+                </button>
+                <p class="CurrentPosition">`
+                    +(ArrayPosition + 1)+'/'+PHOTO_COLLECTION.length+
+                `</p>
+                <button class="ArrowNavigation" id="next-picture" tabindex="-1" onclick=nextPicture(`+(ArrayPosition+1)+`) aria-label="next picture">
+                    >>
+                </button>
+            </nav>`;
 };
